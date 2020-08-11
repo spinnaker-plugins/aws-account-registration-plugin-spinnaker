99e23b551ebb5e3a73acb53fbe283693b487c71d
Manabu McCloskey <mccloman@amazon.com>
2020-08-11 13:42:02 -0600

    Pull account conversion out of AmazonPollingSynchronizer

--- account-registration/src/main/java/com/amazon/aws/spinnaker/plugin/registration/AmazonPollingSynchronizer.java
+++ account-registration/src/main/java/com/amazon/aws/spinnaker/plugin/registration/AmazonPollingSynchronizer.java
@@ -98,7 +98,7 @@ class AmazonPollingSynchronizer {
             return;
         }
         // convert to credentialsConfig from received response.
-        AccountsStatus status = convertCredentials(response.accounts);
+        AccountsStatus status = response.getAccountStatus();
         buildDesiredAccountConfig(status);
         // Sync Amazon credentials in repo
         // CANNOT use defaultAmazonAccountsSynchronizer. Otherwise it will remove ECS accounts everytime there is a change
@@ -140,77 +140,6 @@ class AmazonPollingSynchronizer {
         }
 
         lastSyncTime = response.bookmark;
-    }
-
-    private ECSCredentialsConfig.Account makeECSAccount(Account account) {
-        ECSCredentialsConfig.Account ecsAccount = new ECSCredentialsConfig.Account();
-        ecsAccount.setAwsAccount(account.getName());
-        ecsAccount.setName(account.getName() + "-ecs");
-        return ecsAccount;
-    }
-
-    private CredentialsConfig.Account makeEC2Account(Account account) {
-        List<CredentialsConfig.Region> regions = new ArrayList<>();
-        for (String region : account.getRegions()) {
-            CredentialsConfig.Region regionToAdd = new CredentialsConfig.Region();
-            regionToAdd.setName(region);
-            regions.add(regionToAdd);
-        }
-        CredentialsConfig.Account ec2Account = new CredentialsConfig.Account() {{
-            setName(account.getName());
-            setAccountId(account.getAccountId());
-            setAssumeRole(account.getAssumeRole());
-            setRegions(regions);
-            setAccountType(account.getType());
-            setPermissions(account.getPermissions());
-            setEnvironment(account.getEnvironment());
-            setDefaultKeyPair(account.getDefaultKeyPair());
-            setDefaultSecurityGroups(account.getDefaultSecurityGroups());
-        }};
-        return ec2Account;
-    }
-
-    private AccountsStatus convertCredentials(List<Account> accounts) {
-        AccountsStatus status = new AccountsStatus();
-        // in case duplicate account names were given.
-        List<String> processed = new ArrayList<>();
-        List<CredentialsConfig.Account> ec2AccountsToAdd = new ArrayList<>();
-        List<ECSCredentialsConfig.Account> ecsAccountsToAdd = new ArrayList<>();
-        List<String> deletedAccounts = new ArrayList<>();
-        for (Account account : accounts) {
-            if (processed.contains(account.getName())) {
-                continue;
-            }
-            if (account.getDeletedAt() != null && account.getDeletedAt() != 0) {
-                deletedAccounts.add(account.getName());
-                continue;
-            }
-            CredentialsConfig.Account ec2Account = makeEC2Account(account);
-            if (account.getProviders().isEmpty()) {
-                // enable ecs, and lambda
-                ec2Account.setLambdaEnabled(true);
-                ec2AccountsToAdd.add(ec2Account);
-                ecsAccountsToAdd.add(makeECSAccount(account));
-                processed.add(account.getName());
-                continue;
-            }
-            for (String provider : account.getProviders()) {
-                if ("lambda".equals(provider)) {
-                    ec2Account.setLambdaEnabled(true);
-                    break;
-                }
-                if ("ecs".equals(provider)) {
-                    ecsAccountsToAdd.add(makeECSAccount(account));
-                    break;
-                }
-            }
-            ec2AccountsToAdd.add(ec2Account);
-            processed.add(account.getName());
-        }
-        status.setDeletedAccounts(deletedAccounts);
-        status.setEc2Accounts(ec2AccountsToAdd);
-        status.setEcsAccounts(ecsAccountsToAdd);
-        return status;
     }
 
     private Response getResourceFromRemoteHost(String url) {
--- account-registration/src/main/java/com/amazon/aws/spinnaker/plugin/registration/Response.java
+++ account-registration/src/main/java/com/amazon/aws/spinnaker/plugin/registration/Response.java
@@ -17,6 +17,8 @@
 
 package com.amazon.aws.spinnaker.plugin.registration;
 
+import com.netflix.spinnaker.clouddriver.aws.security.config.CredentialsConfig;
+import com.netflix.spinnaker.clouddriver.ecs.security.ECSCredentialsConfig;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -30,4 +32,80 @@ public class Response {
 
     List<Account> accounts;
     Long bookmark;
+
+    public AccountsStatus getAccountStatus() {
+        return convertCredentials(accounts);
+    }
+
+    private ECSCredentialsConfig.Account makeECSAccount(Account account) {
+        ECSCredentialsConfig.Account ecsAccount = new ECSCredentialsConfig.Account();
+        ecsAccount.setAwsAccount(account.getName());
+        ecsAccount.setName(account.getName() + "-ecs");
+        return ecsAccount;
+    }
+
+    private CredentialsConfig.Account makeEC2Account(Account account) {
+        List<CredentialsConfig.Region> regions = new ArrayList<>();
+        for (String region : account.getRegions()) {
+            CredentialsConfig.Region regionToAdd = new CredentialsConfig.Region();
+            regionToAdd.setName(region);
+            regions.add(regionToAdd);
+        }
+        CredentialsConfig.Account ec2Account = new CredentialsConfig.Account() {{
+            setName(account.getName());
+            setAccountId(account.getAccountId());
+            setAssumeRole(account.getAssumeRole());
+            setRegions(regions);
+            setAccountType(account.getType());
+            setPermissions(account.getPermissions());
+            setEnvironment(account.getEnvironment());
+            setDefaultKeyPair(account.getDefaultKeyPair());
+            setDefaultSecurityGroups(account.getDefaultSecurityGroups());
+        }};
+        return ec2Account;
+    }
+
+    private AccountsStatus convertCredentials(List<Account> accounts) {
+        AccountsStatus status = new AccountsStatus();
+        // in case duplicate account names were given.
+        List<String> processed = new ArrayList<>();
+        List<CredentialsConfig.Account> ec2AccountsToAdd = new ArrayList<>();
+        List<ECSCredentialsConfig.Account> ecsAccountsToAdd = new ArrayList<>();
+        List<String> deletedAccounts = new ArrayList<>();
+        for (Account account : accounts) {
+            if (processed.contains(account.getName())) {
+                continue;
+            }
+            if (account.getDeletedAt() != null && account.getDeletedAt() != 0) {
+                deletedAccounts.add(account.getName());
+                continue;
+            }
+            CredentialsConfig.Account ec2Account = makeEC2Account(account);
+            if (account.getProviders().isEmpty()) {
+                // enable ecs, and lambda
+                ec2Account.setLambdaEnabled(true);
+                ec2AccountsToAdd.add(ec2Account);
+                ecsAccountsToAdd.add(makeECSAccount(account));
+                processed.add(account.getName());
+                continue;
+            }
+            for (String provider : account.getProviders()) {
+                if ("lambda".equals(provider)) {
+                    ec2Account.setLambdaEnabled(true);
+                    break;
+                }
+                if ("ecs".equals(provider)) {
+                    ecsAccountsToAdd.add(makeECSAccount(account));
+                    break;
+                }
+            }
+            ec2AccountsToAdd.add(ec2Account);
+            processed.add(account.getName());
+        }
+        status.setDeletedAccounts(deletedAccounts);
+        status.setEc2Accounts(ec2AccountsToAdd);
+        status.setEcsAccounts(ecsAccountsToAdd);
+        return status;
+    }
+
 }
